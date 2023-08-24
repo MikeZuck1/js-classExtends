d9b3c6adcb36d9683fe717c87d9d85140ead5ef5
MikeZuck1 <105445747+MikeZuck1@users.noreply.github.com>
2023-08-25 00:14:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-classExtends
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-4tx71s)
---
+++ index.js
@@ -0,0 +1,47 @@
+// Import stylesheets
+import './style.css';
+
+const appDiv = document.getElementById('app');
+appDiv.innerHTML;
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  } 
+
+  get certifications() {
+    return this._certifications;
+  }
+
+  addCertification(newCertification) {
+    this._certifications.push(newCertification); 
+  }
+} 
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
+// should
